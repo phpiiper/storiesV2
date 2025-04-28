5203b8affd669e9c136f1ba5600e5deeb86eb85b
Piper Phair <113475156+phpiiper@users.noreply.github.com>
2025-04-27 21:53:08 -0400

    Add files via upload

---
+++ frontend/library.js
@@ -0,0 +1,9 @@
+import HomeBar from "@/components/HomeBar";
+
+export default function Library() {
+    return (<><HomeBar /><div id={"library-page"}>
+        <h1>Library</h1>
+        <p>Will be implemented soon</p>
+        <p>The place for bookmarked stories for them to show up here</p>
+    </div></>)
+}
